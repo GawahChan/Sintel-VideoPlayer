de5d5b6565a31e27933a585959a21c0ff608db43
Gawah Chan <gawahchan@gmail.com>
2019-10-30 19:28:02 +0000

    Added framer-motion library

--- src/components/videoplayer/ProgressBar.js
+++ src/components/videoplayer/ProgressBar.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { ProgressBarContainer, Bar } from './styled';
 
 function ProgressBar({ status }) {
-  console.log(status);
   return (
     <ProgressBarContainer>
       <Bar Size={status} />
