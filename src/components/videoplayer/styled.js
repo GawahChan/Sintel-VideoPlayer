f95151f9456677540c9302c835d8e2f84db5a2d7
Gawah Chan <gawahchan@gmail.com>
2019-10-31 11:58:09 +0000

    Created marker for the progress bar

--- src/components/videoplayer/styled.js
+++ src/components/videoplayer/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export const Container = styled.div`
   padding: 5px;
@@ -18,15 +19,26 @@ export const ControlsContainer = styled.div`
   padding: 5px;
 `;
 
-export const ProgressBarContainer = styled.div`
+export const ProgressBarContainer = styled(motion.div)`
   width: 100%;
   background-color: darkgray;
   height: 3px;
 `;
 
-export const Bar = styled.div`
+export const Bar = styled(motion.div)`
+  position: relative;
   background-color: red;
   height: 3px;
   width: ${props => props.Size || `2px`};
+`;
+
+export const Marker = styled.div`
+  position: absolute;
+  right: -5px;
+  top: -3px;
+  height: 10px;
+  width: 10px;
+  background-color: red;
+  border-radius: 50%;
   z-index: 1;
 `;
