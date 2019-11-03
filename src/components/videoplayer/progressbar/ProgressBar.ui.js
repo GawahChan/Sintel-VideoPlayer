0e139f2e26f40b90bef70cb4b86e5d0cb5976239
Gawah Chan <gawahchan@gmail.com>
2019-11-03 20:08:48 +0000

    Added more hotspots to the video

--- src/components/videoplayer/progressbar/ProgressBar.ui.js
+++ src/components/videoplayer/progressbar/ProgressBar.ui.js
@@ -74,19 +74,45 @@ function ProgressBar({
     setDisplayPreview(displayPreview);
   };
 
+  console.log('time', currentVideoTime);
+
   return (
     <ProgressBarContainer ref={progressBarRef}>
       <HotSpot
         updateProgressBar={progressBarPosition}
         progressBarWidth={progressBarWidth}
         hotSpotTimeStamp={25}
-        hotSpotText="Winter has come!"
+        hotSpotText="Skip to beginning!"
+      />
+      <HotSpot
+        updateProgressBar={progressBarPosition}
+        progressBarWidth={progressBarWidth}
+        hotSpotTimeStamp={148}
+        hotSpotText="Flashback Scene"
+      />
+      <HotSpot
+        updateProgressBar={progressBarPosition}
+        progressBarWidth={progressBarWidth}
+        hotSpotTimeStamp={345}
+        hotSpotText="Epic Journey Montage"
+      />
+      <HotSpot
+        updateProgressBar={progressBarPosition}
+        progressBarWidth={progressBarWidth}
+        hotSpotTimeStamp={539}
+        hotSpotText="Dracarys!"
       />
       <HotSpot
         updateProgressBar={progressBarPosition}
         progressBarWidth={progressBarWidth}
         hotSpotTimeStamp={736}
-        hotSpotText="Credits!"
+        hotSpotText="Skip to credits!"
+      />
+      <HotSpot
+        updateProgressBar={progressBarPosition}
+        progressBarWidth={progressBarWidth}
+        hotSpotTimeStamp={885}
+        hotSpotText="Check out sintel.org for more info about this film!"
       />
       <BarContainer onTap={event => progressBarPosition(event)} />
       <Bar Size={progressBarSize} />
